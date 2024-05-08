050175309c6e6632ad457fafed5d738606d2ae42
Sneh Arora <sneh@adobe.com>
2024-05-08 14:30:56 +0530

    Footer changes

--- scripts/blocks-utils.js
+++ scripts/blocks-utils.js
@@ -1,4 +1,5 @@
-function createCustomElement(tagname, className) {
+
+function createElement(tagname, className) {
   const element = document.createElement(tagname);
   if (className) {
     element.classList.add(className);
@@ -6,23 +7,47 @@ function createCustomElement(tagname, className) {
   return element;
 }
 
-async function fetchData(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+/**
+ * Reads the block markup and returns the configuration object.
+ * This function returns the second column of each row as the value for the first column.
+ * If there are more than two columns in a row, the function ignores the rest of the columns.
+ * but those can be retrived by nextSibling property of the value column.
+ * @param block - The block element.
+ * @returns {{}} - The configuration object.
+ */
+function readBlockMarkup(block) {
+  const config = {};
+  block.querySelectorAll(':scope > div').forEach((row) => {
+    if (row.children) {
+      const cols = [...row.children];
+      if (cols[1]) {
+        const col = cols[1];
+        const name = toClassName(cols[0].textContent);
+        config[name] = col;
+      }
     }
-    const jsonData = await response.json(); // Wait for JSON parsing
-    return jsonData.data.map((country) => ({
-      name: country.name,
-      url: country.url,
-    }));
-  } catch (error) {
-    return null;
-  }
+  });
+  return config;
+}
+
+function fetchData(url) {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return data.data.map((country) => ({
+        name: country.name,
+        link: country.url,
+      }));
+    })
+    .catch((error) => {
+      callback(error, null);
+    });
 }
 
 export {
-  createCustomElement,
-  fetchData,
+  createElement,
+  readBlockMarkup,
+  fetchData 
 };
