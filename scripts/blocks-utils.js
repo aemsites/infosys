f1e64f9dbf990beb52fe15723d44413a3810ad06
Sneh Arora <sneh@adobe.com>
2024-05-09 18:38:09 +0530

    footer-reading values from doc
    footer-reading values from doc
    
    footer

--- scripts/blocks-utils.js
+++ scripts/blocks-utils.js
@@ -6,29 +6,6 @@ function createElement(tagname, className) {
   return element;
 }
 
-/**
- * Reads the block markup and returns the configuration object.
- * This function returns the second column of each row as the value for the first column.
- * If there are more than two columns in a row, the function ignores the rest of the columns.
- * but those can be retrived by nextSibling property of the value column.
- * @param block - The block element.
- * @returns {{}} - The configuration object.
- */
-function readBlockMarkup(block) {
-  const config = {};
-  block.querySelectorAll(':scope > div').forEach((row) => {
-    if (row.children) {
-      const cols = [...row.children];
-      if (cols[1]) {
-        const col = cols[1];
-        const name = toClassName(cols[0].textContent);
-        config[name] = col;
-      }
-    }
-  });
-  return config;
-}
-
 async function fetchData(url) {
   try {
     const response = await fetch(url);
@@ -38,7 +15,7 @@ async function fetchData(url) {
     const jsonData = await response.json(); // Wait for JSON parsing
     return jsonData.data.map((country) => ({
       name: country.name,
-      link: country.url,
+      url: country.url,
     }));
   } catch (error) {
     return null;
@@ -47,6 +24,5 @@ async function fetchData(url) {
 
 export {
   createElement,
-  readBlockMarkup,
   fetchData,
 };
