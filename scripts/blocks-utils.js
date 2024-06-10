ccce8ea76583a2609d039743091dcbfae9aeb655
Anuj Mahajan <anujmaha@adobe.com>
2024-06-10 14:10:30 +0530

    accordion block optimised image src

--- scripts/blocks-utils.js
+++ scripts/blocks-utils.js
@@ -46,8 +46,39 @@ async function fetchData(url) {
   }
 }
 
+function getOptimalImageFromPictureTag(picture) {
+  if (!picture) return '';
+
+  const sources = picture.querySelectorAll('source');
+  let selectedSrc = '';
+
+  if (sources && sources.length > 0) {
+    // Loop through source elements to find the first matching media query
+    for (const source of sources) {
+      if (source.media && window.matchMedia(source.media).matches) {
+        selectedSrc = source.srcset;
+        break;
+      } else if (!source.media) {
+        // If no media attribute is present, it's the default source
+        selectedSrc = source.srcset;
+      }
+    }
+  }
+
+  // If no media query matches, use the img element's src as fallback
+  if (!selectedSrc) {
+    const img = picture.querySelector('img');
+    if (img) {
+      selectedSrc = img.currentSrc || img.src;
+    }
+  }
+
+  return selectedSrc;
+}
+
 export {
   createAemElement,
   createCustomElement,
   fetchData,
+  getOptimalImageFromPictureTag,
 };
