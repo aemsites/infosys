33b26d5ca2ca41c9c132112b8d7911025ff29daa
Anuj Mahajan <66720268+anuj-adobe@users.noreply.github.com>
2024-11-13 20:04:03 +0530

    Accordion block refactored (#105)

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -2,6 +2,7 @@ import { createAemElement, getOptimalImageFromPictureTag } from '../../scripts/b
 
 const ACCORDION_TITLE_PSEUDO_BG_IMAGE = '--accordion-pseudo-title-bg-image';
 const ACCORDION_ITEM_SWITCH_INTERVAL = 5000; // milliseconds
+let totalItems = 0;
 
 function getActiveItemIndex(block) {
   let activeItemIndex = block.getAttribute('data-active-item');
@@ -15,22 +16,20 @@ function setActiveItem(block, index) {
   block.setAttribute('data-active-item', index);
 }
 
-function getNthItemTitle(block, n) {
-  const titles = block.querySelectorAll('.item-title');
-  return titles[n];
+function getActiveListItem(block) {
+  const activeIndex = getActiveItemIndex(block);
+  return block.querySelector(`ul > li:nth-child(${activeIndex + 1})`);
 }
 
-function toggleActiveItem(block) {
-  const activeIndex = getActiveItemIndex(block);
-  const title = getNthItemTitle(block, activeIndex);
-  title.classList.toggle('open');
+function getNthListItem(block, n) {
+  return block.querySelector(`:scope > ul > li:nth-child(${n + 1})`);
 }
 
 function handleAccordionSwitch(block) {
-  const titles = block.querySelectorAll('.item-title');
   const currentIndex = getActiveItemIndex(block);
-  const openIndex = (currentIndex + 1) % titles.length;
-  titles[openIndex].click();
+  const newIndex = (currentIndex + 1) % totalItems;
+  const nthListItem = getNthListItem(block, newIndex);
+  nthListItem.click();
 }
 
 function setTitleBgImg(titleDiv, itemContent) {
@@ -56,62 +55,62 @@ function decorateContentLink(contentMainDiv) {
   }
 }
 
-function decorateAccordionContent(block) {
-  const contents = block.querySelectorAll('.item-content');
-  contents.forEach((content) => {
-    const children = [...content.children];
-    const imageDiv = children[0];
-    const contentMainDiv = children[1];
-    content.classList.add('overlay');
-    imageDiv.classList.add('item-content-image');
-    contentMainDiv.classList.add('item-content-main');
-    const contentTitle = contentMainDiv.querySelector('h3');
-    const titleDiv = content.previousElementSibling;
-    const title = titleDiv ? titleDiv.querySelector('h4') : '';
-    if (!contentTitle && title) {
-      const h3 = createAemElement('h3', { class: 'item-content-title' });
-      h3.textContent = title.textContent;
-      contentMainDiv.prepend(h3);
-    }
-    decorateContentLink(contentMainDiv);
-    setTitleBgImg(titleDiv, content);
-  });
-  return contents;
+function decorateContent(block, content) {
+  if (!content) return;
+  const children = [...content.children];
+  const imageDiv = children[0];
+  const contentMainDiv = children[1];
+  content.classList.add('item-content', 'overlay');
+  imageDiv.classList.add('item-content-image');
+  contentMainDiv.classList.add('item-content-main');
+  const contentTitle = contentMainDiv.querySelector('h3');
+  const titleDiv = content.previousElementSibling;
+  const title = titleDiv ? titleDiv.querySelector('h4') : '';
+  if (!contentTitle && title) {
+    const h3 = createAemElement('h3', { class: 'item-content-title' });
+    h3.textContent = title.textContent;
+    contentMainDiv.prepend(h3);
+  }
+  decorateContentLink(contentMainDiv);
+  setTitleBgImg(titleDiv, content);
 }
 
-function decorateAccordionTitles(block) {
+function createAccordionList(block) {
+  const ul = createAemElement('ul', { class: 'list' });
   const titles = block.querySelectorAll(':scope > div:nth-child(odd)');
+  totalItems = titles.length;
   titles.forEach((title, index) => {
+    const li = createAemElement('li', { class: 'item' });
     title.classList.add('item-title', 'overlay');
+    const content = title.nextElementSibling;
+    decorateContent(block, content);
+    li.append(title, content);
     if (index === 0) {
-      title.classList.add('open');
-      setActiveItem(block, index);
+      li.classList.add('active');
+      setActiveItem(block, 0);
     }
-    title.nextElementSibling.classList.add('item-content');
-
-    title.addEventListener('click', () => {
-      toggleActiveItem(block);
+    li.addEventListener('click', () => {
+      const currentActiveItem = getActiveListItem(block);
+      currentActiveItem.classList.toggle('active');
       clearInterval(block.intervalId);
       block.intervalId = setInterval(
         () => handleAccordionSwitch(block),
         ACCORDION_ITEM_SWITCH_INTERVAL,
       );
-      title.classList.toggle('open');
+      li.classList.add('active');
       setActiveItem(block, index);
     });
+    ul.append(li);
   });
-  return titles;
+  return ul;
 }
 
-function decorateAccordion(block) {
-  decorateAccordionTitles(block);
-  decorateAccordionContent(block);
+export default function decorate(block) {
+  const ul = createAccordionList(block);
+  block.innerHTML = '';
+  block.append(ul);
   block.intervalId = setInterval(
     () => handleAccordionSwitch(block),
     ACCORDION_ITEM_SWITCH_INTERVAL,
   );
-}
-
-export default function decorate(block) {
-  decorateAccordion(block);
 }
